952ec2fd0707559ffd29b9c4bfec8566677e8ff1
duha94 <duhahindi@live.com>
2025-02-11 16:05:34 +0300

    added Desktop design

--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import './index.css';
 
-export const Button = ({ onClick, children, className }) => {
+export const Button = ({ onClick, children, className, icon }) => {
   return (
     <button
       onClick={onClick}
       className={`px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition ${className}`}
     >
-      {children}
+      {icon && <img src={icon} className='icon'/> || null} {children}
     </button>
   );
 };
