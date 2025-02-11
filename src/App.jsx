a16b7ca25c457fc5f8da9e3bc4b5336c703cda2e
duha94 <duhahindi@live.com>
2025-02-11 16:15:14 +0300

    Added SpeedInsights

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./App.css";
 import Home from "./pages/Home/home";
 
@@ -9,6 +10,7 @@ export default function CaseConverter() {
     <div className="App">
       <Home />
       <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
